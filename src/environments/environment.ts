8768a4961590f2c8a87399f945feef9cc59a25b9
Krystal Cheung <krycheung@gmail.com>
2023-05-03 12:55:37 -0400

    add comment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,4 @@
+// Improvement needed: Store API key in server side to prevent the public from accessing it
 export const environment = {
     apiUrl: 'https://api.pexels.com/v1/',
     apiKey: 'eZji5h1Ie55Dxk7FCdycI0hZdLwp4Or5twRaTTj5mYCJ3W87Wvr2K19M'
